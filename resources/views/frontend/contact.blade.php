0d5dbf8d5f25edb9dc67332fda43789b6b739a47
Quangvv <quangvv@vinsofts.net>
2018-10-25 17:48:35 +0700

    up len

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -2,40 +2,6 @@
 @section('title', 'Blog')
 @section('Content')
 <div class="container-mot" style="margin-left:20%; margin-right:20%;">
-
-<h1><span style="font-size:36px"><strong>Li&ecirc;n hệ</strong></span></h1>
-
-<img class="img-logo" alt="" src="/uploads/browse/images/logo(3).jpg" style="height:243px; width:412px" />
-
-<div class="box entry-content mark-links post-content">
-<p><span style="font-size:18px"><strong>Ng&acirc;n H&agrave;ng Vietcombank chi nh&aacute;nh T&acirc;y Hồ&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong></span></p>
-
-<p><span style="font-size:18px">Số t&agrave;i khoản: 0991000007854</span></p>
-
-<p><span style="font-size:18px">T&ecirc;n t&agrave;i khoản : B&ugrave;i Thị Thủy</span></p>
-
-<p><span style="font-size:18px">Sở giao dịch&nbsp;H&agrave; Nội</span></p>
-
-<p><span style="font-size:18px"><strong>Ng&acirc;n h&agrave;ng Agribank chi nh&aacute;nh H&agrave; Th&agrave;nh&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong></span></p>
-
-<p><span style="font-size:18px">Số t&agrave;i khoản: 1303206122800</span></p>
-
-<p><span style="font-size:18px">T&ecirc;n t&agrave;i khoản: B&ugrave;i Thị Thủy</span></p>
-
-<h2><span style="font-size:18px"><strong>C&ocirc;ng ty TNHH sản xuất v&agrave; thương mại Tuấn Nguy&ecirc;n&nbsp;</strong></span></h2>
-
-<p><span style="font-size:18px">Nh&agrave; cung cấp uy t&iacute;n</span></p>
-
-<p><span style="font-size:18px">Địa chỉ: Số nh&agrave; 12,d&atilde;y B,khu 105 đường Lạc Long Qu&acirc;n,T&acirc;y Hồ,H&agrave; Nội</span></p>
-
-<p><span style="font-size:18px">Điện thoại:&nbsp;01657137288 &ndash; 0973387133</span></p>
-
-<p><span style="font-size:18px">Email:&nbsp;thamototuannguyen@gmail.com</span></p>
-</div></div>
-<style>
-.img-logo{
-    float:right;
-}
-</style>
-
+{!!@Settings::get('long_description1')!!} 
+</div>
 @endsection
