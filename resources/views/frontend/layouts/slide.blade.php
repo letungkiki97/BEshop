8e8728a87fe423aec492b08a36cda82ada662863
Lê Thanh Tùng <tunglt.dev@gmail.com>
2018-10-25 22:50:14 +0700

    add active nav home

--- resources/views/frontend/layouts/slide.blade.php
+++ resources/views/frontend/layouts/slide.blade.php
@@ -3,23 +3,25 @@
     $banner = App\Models\Banner::orderByRaw("RAND()")->first();
     @endphp
     <div id="owl-main-slider" class="owl-carousel owl-theme">
-        @foreach($banner->images as $k=>$v)
+        @if(!empty($banner->images))
+            @foreach($banner->images as $k=>$v)
 
-        <div class="item">
-            <div class="ms-img">
-                <img src="{{url('uploads/banner/'.@$v->name)}}" alt="Các mẫu xe thể thao" />
-                <div class="ms-desc">
-                    <div class="line-1 wow fadeInLeft" data-wow-duration="0.75s" data-wow-delay="0.5s">
-                        {{@$banner->name}}
+            <div class="item">
+                <div class="ms-img">
+                    <img src="{{url('uploads/banner/'.@$v->name)}}" alt="Các mẫu xe thể thao" />
+                    <div class="ms-desc">
+                        <div class="line-1 wow fadeInLeft" data-wow-duration="0.75s" data-wow-delay="0.5s">
+                            {{@$banner->name}}
+                        </div>
+                        <div class="line-2 wow fadeInRight" data-wow-duration="0.75s" data-wow-delay="0.5s">
+                            <p style="color: #{{@$v->pivot->text_color}}">{{@$v->pivot->text_text}}</p>
+                        </div>
+                        <a href="{{@$v->pivot->link}}" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="1.5s">{{@$v->pivot->button_text}}</a>
                     </div>
-                    <div class="line-2 wow fadeInRight" data-wow-duration="0.75s" data-wow-delay="0.5s">
-                        <p style="color: #{{@$v->pivot->text_color}}">{{@$v->pivot->text_text}}</p>
-                    </div>
-                    <a href="{{@$v->pivot->link}}" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="1.5s">{{@$v->pivot->button_text}}</a>
                 </div>
             </div>
-        </div>
-        @endforeach
+            @endforeach
+        @endif
     </div>
 </div>
 <style>
